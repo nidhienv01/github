f686a4e3e2331929bb0b421b7d7fc6ea5bd87d07
Nidhi Shukla <118169817+nidhienv01@users.noreply.github.com>
2022-11-14 13:00:45 +0000

    Add change

--- dlnm_code.R
+++ dlnm_code.R
@@ -1,9 +1,13 @@
 #github
 library(usethis)
+create_github_token()
+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
 library(gitcreds)
 gitcreds_set()
-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
+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
+
 use_github()
+
 #code
 
 library(dlnm)
